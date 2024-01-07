9d7852d2ee107d60fd9d4f7ca98358e1e77e4c09
DongZirui <dandrezereph@outlook.com>
2024-01-07 11:05:52 +0800

    修bug

--- telegram_bot.py
+++ telegram_bot.py
@@ -24,7 +24,7 @@ def return_massage(text,command,message):
 def webhook():
     json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
+    bot.process_new_updates(update)
     return '', 200
 @app.route('/', methods=['GET'])
 def set():
